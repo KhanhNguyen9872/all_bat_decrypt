d6bf92bc38d18cb40851e61f6d9e2ba4c8b7c504
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-10-16 14:06:19 +0700

    Add files via upload

---
+++ ntdcuti_Protected.bat
@@ -0,0 +1,55 @@
+@echo off
+Title . 
+cd %encoder_ntdencoder_ntdencoder_ntdencoder_ntdencoder_ntdencoder_ntdencoder_ntdencoder_ntdencoder_ntdencoder_ntd\system32
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "CPU" /t REG_DWORD /d "100" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "DPI" /t REG_DWORD /d "240" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "Fov" /t REG_DWORD /d "32000501" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "generalemulatorsensitivity" /t REG_DWORD /d "100" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "GPU" /t REG_DWORD /d "100" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "joystick" /t REG_DWORD /d "1000" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "LEFTCLICK" /t REG_DWORD /d "1000" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "sensitivity" /t REG_DWORD /d "100" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "SMALLESTWIDHT" /t REG_DWORD /d "750" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "speedofmovement" /t REG_DWORD /d "100" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "touchsensitivyty" /t REG_DWORD /d "450" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "X" /t REG_DWORD /d "1670" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "Y" /t REG_DWORD /d "500" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "SMALLESTWIDTH" /t REG_DWORD /d "750" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "Touch&3Dsensitive" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "Look around sensitive" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "IP-speed" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "IA-deplay" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "3D-center" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "freefireMax" /t REG_DWORD /d "4608" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "freefire" /t REG_DWORD /d "24576" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "host" /t REG_DWORD /d "8192" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "touch adaptation" /t REG_DWORD /d "512" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "swipe gesture" /t REG_DWORD /d "80" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /v "Touch&3Dsensitive" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "CPU" /t REG_DWORD /d "100" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "DPI" /t REG_DWORD /d "440" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "Fov" /t REG_DWORD /d "60" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "generalemulatorsensitivity" /t REG_DWORD /d "100" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "GPU" /t REG_DWORD /d "100" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "joystick" /t REG_DWORD /d "1000" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "LEFTCLICK" /t REG_DWORD /d "1000" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "sensitivity" /t REG_DWORD /d "100" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "SMALLESTWIDTH" /t REG_DWORD /d "750" /f
+echo Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "touchsensitivyty" /t REG_DWORD /d "720" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "X" /t REG_DWORD /d "1100" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "SMALLESTWIDHT" /t REG_DWORD /d "750" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "Y" /t REG_DWORD /d "256" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "Touch&3Dsensitive" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "Look around sensitive" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "IP-speed" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "IA-deplay" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "3D-center" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "freefireMax" /t REG_DWORD /d "4608" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "freefire" /t REG_DWORD /d "24576" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "host" /t REG_DWORD /d "8192" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "touch adaptation" /t REG_DWORD /d "512" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "swipe gesture" /t REG_DWORD /d "80" /f
+Reg.exe add "HKLM\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /v "Touch&3Dsensitive" /t REG_DWORD /d "0" /f
+echo.
+echo Success
+timeout 10
